5ab6db84b48086c4d725a47d109ce789adb1651d
DhirajNaik <dhirajnaik39@gmail.com>
2022-01-06 13:26:10 +0530

    added code to send to backend.

--- src/components/SimpleDropZone.js
+++ src/components/SimpleDropZone.js
@@ -1,43 +1,95 @@
 import axios from "axios";
 import React from "react";
-// import axios from "axios";
 
+
+// components/simple-dropzone.component.js
 import Dropzone from "react-dropzone-uploader";
 import "react-dropzone-uploader/dist/styles.css";
+import { getDroppedOrSelectedFiles } from 'html5-file-selector'
+
+
+
+const sendDataToBackend = async (filesSubmittedByUser) => {
+    const body = {
+        name: "testNAme",
+        bio: "Test bio123",
+        files: filesSubmittedByUser
+    }
+
+    const response = await axios.post('https://httpbin.org/post', JSON.stringify(body));
+    const json = await response.json();
+    console.log(`data: ${json}`);
+}
+
 
 const SimpleDropZone = () => {
-  // Payload data and url to upload files
-  const getUploadParams = ({ meta }) => {
-    return { url: "https://httpbin.org/post" };
-  };
 
-  // Return the current status of files being uploaded
-  const handleChangeStatus = ({ meta, file }, status) => {
-    console.log(status, meta, file);
-  };
+    const getUploadParams = ({ meta }) => {
+        console.log(meta);
+        return { url: 'https://httpbin.org/post' }
+    }
 
-  // Return array of uploaded files after submit button is clicked
-  const handleSubmit = (files, allFiles) => {
-    console.log(files.map((f) => f.meta));
-    // allFiles.forEach((f) => f.remove());
-    axios
-      .post("https://v2.convertapi.com/upload", files)
-      .then((res) => {
-        console.log("file Uploaded", res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+    const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
 
-  return (
-    <Dropzone
-      getUploadParams={getUploadParams}
-      onChangeStatus={handleChangeStatus}
-      onSubmit={handleSubmit}
-      accept="image/*,audio/*,video/*"
-    />
-  );
+    const handleSubmit = (files, allFiles) => {
+        // console.log(files.map(f => f.meta))
+        // allFiles.forEach(f => f.remove())
+
+        sendDataToBackend(files);
+
+        console.log("Hello...from handleSubmit")
+        console.log('Files submitted:');
+        console.log(allFiles);
+
+    }
+
+    const getFilesFromEvent = e => {
+        return new Promise(resolve => {
+            getDroppedOrSelectedFiles(e).then(chosenFiles => {
+                resolve(chosenFiles.map(f => f.fileObject))
+            })
+        })
+    }
+
+    const InputChooseFile = ({ accept, onFiles, files, getFilesFromEvent }) => {
+        const text = files.length > 0 ? 'Add more files' : 'Choose files to upload'
+
+        const buttonStyle = {
+            backgroundColor: '#67b0ff',
+            color: '#fff',
+            cursor: 'pointer',
+            padding: 15,
+            borderRadius: 30
+        }
+
+        return (
+            <label style={buttonStyle}>
+                {text}
+                <input
+                    style={{ display: 'none' }}
+                    type="file"
+                    accept={accept}
+                    multiple
+                    onChange={e => {
+                        getFilesFromEvent(e).then(chosenFiles => {
+                            onFiles(chosenFiles)
+                        })
+                    }}
+                />
+            </label>
+        )
+    }
+
+    return (
+        <Dropzone
+            getUploadParams={getUploadParams}
+            onChangeStatus={handleChangeStatus}
+            onSubmit={handleSubmit}
+            InputComponent={InputChooseFile}
+            getFilesFromEvent={getFilesFromEvent}
+            classNames
+        />
+    );
 };
 
 export default SimpleDropZone;
